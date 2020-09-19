6db863ec834b2940c83f5c7d8fbae80cc04cf12a
Binay Dhawa <binaydhawa@outlook.com>
2020-09-20 03:38:31 +1000

    Updates

---
+++ precache-manifest.593513a89b2a82ea0eff889ab2253807.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fd0d4fa959527e0aeb944d6a98d2235",
+    "url": "/Quizzme/index.html"
+  },
+  {
+    "revision": "a3c5a0ff3d0e6d8b2ec6",
+    "url": "/Quizzme/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "4863bef3ed1ba19bb77c",
+    "url": "/Quizzme/static/css/main.1797f2cd.chunk.css"
+  },
+  {
+    "revision": "a3c5a0ff3d0e6d8b2ec6",
+    "url": "/Quizzme/static/js/2.d94aa3be.chunk.js"
+  },
+  {
+    "revision": "f2efbe82e3ec6b086295954b52317f8c",
+    "url": "/Quizzme/static/js/2.d94aa3be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4863bef3ed1ba19bb77c",
+    "url": "/Quizzme/static/js/main.9f316726.chunk.js"
+  },
+  {
+    "revision": "3eb29dab0e3ac028a3f3",
+    "url": "/Quizzme/static/js/runtime-main.4bb4c2df.js"
+  }
+]);
